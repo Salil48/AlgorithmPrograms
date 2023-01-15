aff5537edd1359643eaf33ad2af2ca85e0e15e43
“Salil48” <salilchharia@gmail.com>
2023-01-15 12:53:12 +0530

    Insertion Sort

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@
             {
                 Console.WriteLine("1.Permutation Using Recursion");
                 Console.WriteLine("2.Binary Search the word from wordlist");
+                Console.WriteLine("3.Insertion Sort");
                 Console.WriteLine("Enter the option");
                 int num = Convert.ToInt32(Console.ReadLine());
                 switch (num)
@@ -22,15 +23,20 @@
                         Permutations.Permutate(str, 0, n - 1);
                         break;
                     case 2:
-                        string txtpath = File.ReadAllText(@"J:\240\AlgorithmPrograms\AlgorithmPrograms\algo.txt");
-                        List<string> words = new List<string>(txtpath.Split());
+                        string txtpath = File.ReadAllText(@"C:\Users\Radhika\source\repos\AlgorithmPrograms1\ListOfWords.txt");
+                        List<string> words = new List<string>(txtpath.Split(" "));
                         words.Sort();
                         BinarySearch.BinarySearchWord(words);
+                        break;
+                    case 3:
+                        InsertionSort insertionSort = new InsertionSort();
+                        int[] arr = { 10, 2, 14, 3, 1, 5, 9 };
+                        insertionSort.Sort(arr);
+                        insertionSort.PrintArray(arr);
                         break;
                     default:
                         Console.WriteLine("Enter the valid option!");
                         break;
-
                 }
                 Console.WriteLine("\nDo you want to continue");
                 flag = Console.ReadLine();
@@ -38,3 +44,12 @@
         }
     }
 }
+
+
+
+
+
+
+        
+
+    
