8f2c6f5a27de299d213c87e763a59c88a15744f0
“Salil48” <salilchharia@gmail.com>
2023-01-15 12:22:21 +0530

    Welcome to Algorithm Programs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AlgorithmPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            
+        }
+    }
+}
