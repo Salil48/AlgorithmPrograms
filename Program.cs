c631902a2d4bd4f0ef352fafd6d6826d9dfe4cef
“Salil48” <salilchharia@gmail.com>
2023-01-15 12:27:14 +0530

    Permutation using Recursion

--- Program.cs
+++ Program.cs
@@ -5,7 +5,24 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs!");
-            
+            Console.WriteLine("1.Permutation Using Recursion");
+            Console.WriteLine("Enter the option");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Enter String to find Permutation ");
+                    string str = Console.ReadLine();
+                    int n = str.Length;
+                    Permutations.Permutate(str, 0, n - 1);
+                    break;
+
+                default:
+                    Console.WriteLine("Enter the valid option!");
+                    break;
+            }
+            Console.WriteLine("\nDo you want to continue");
+            Console.ReadLine();
         }
     }
 }
