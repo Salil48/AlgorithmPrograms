a9652bc850e7d946f2b3eaae0be88ebf7e3a63ef
“Salil48” <salilchharia@gmail.com>
2023-01-15 17:01:05 +0530

    Insertion_Sort

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     class Program
     {
 
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs!");
             string flag = "Y";
@@ -12,10 +12,6 @@
                 Console.WriteLine("1.Permutation Using Recursion");
                 Console.WriteLine("2.Binary Search the word from wordlist");
                 Console.WriteLine("3.Insertion Sort");
-                Console.WriteLine("4.Bubble Sort");
-                Console.WriteLine("5.Merge sort");
-                Console.WriteLine("6.Anagram Problem");
-                Console.WriteLine("7.Prime number in range");
                 Console.WriteLine("Enter the option");
                 int num = Convert.ToInt32(Console.ReadLine());
                 switch (num)
@@ -27,7 +23,7 @@
                         Permutations.Permutate(str, 0, n - 1);
                         break;
                     case 2:
-                        string txtpath = File.ReadAllText(@"C:\Users\Radhika\source\repos\AlgorithmPrograms1\ListOfWords.txt");
+                        string txtpath = File.ReadAllText(@"J:\240\AlgorithmPrograms\AlgorithmPrograms\algo.txt");
                         List<string> words = new List<string>(txtpath.Split(" "));
                         words.Sort();
                         BinarySearch.BinarySearchWord(words);
@@ -38,25 +34,6 @@
                         insertionSort.Sort(arr);
                         insertionSort.PrintArray(arr);
                         break;
-                    case 4:
-                        int[] arr1 = { 10, 2, 14, 3, 1, 5, 9 };
-                        BubbleSort.BubbleSortInArray(arr1);
-                        BubbleSort.PrintArray(arr1);
-                        break;
-                    case 5:
-                        int[] arr2 = { 32, 12, 9, 65, 24, 5 };
-                        MergeSort.Sort(arr2, 0, arr2.Length - 1);
-                        MergeSort.PrintArray(arr2);
-                        break;
-                    case 6:
-                        Anagram.FindAnagram();
-                        break;
-                    case 7:
-                        Console.WriteLine("The prime numbers are : ");
-                        PrimeNumInRange primeNum = new PrimeNumInRange();
-                        primeNum.FindPrime();
-                        break;
-
                     default:
                         Console.WriteLine("Enter the valid option!");
                         break;
@@ -64,6 +41,7 @@
                 Console.WriteLine("\nDo you want to continue");
                 flag = Console.ReadLine();
             }
+
         }
     }
 }
