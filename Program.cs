7cdac4adc06d0a6f208aed54d7c5fa0dcd7f1812
“Salil48” <salilchharia@gmail.com>
2023-01-15 12:36:18 +0530

    binary search the word from list

--- Program.cs
+++ Program.cs
@@ -2,27 +2,39 @@
 {
     class Program
     {
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs!");
-            Console.WriteLine("1.Permutation Using Recursion");
-            Console.WriteLine("Enter the option");
-            int num = Convert.ToInt32(Console.ReadLine());
-            switch (num)
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
             {
-                case 1:
-                    Console.WriteLine("Enter String to find Permutation ");
-                    string str = Console.ReadLine();
-                    int n = str.Length;
-                    Permutations.Permutate(str, 0, n - 1);
-                    break;
+                Console.WriteLine("1.Permutation Using Recursion");
+                Console.WriteLine("2.Binary Search the word from wordlist");
+                Console.WriteLine("Enter the option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Enter String to find Permutation ");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        Permutations.Permutate(str, 0, n - 1);
+                        break;
+                    case 2:
+                        string txtpath = File.ReadAllText(@"J:\240\AlgorithmPrograms\AlgorithmPrograms\algo.txt");
+                        List<string> words = new List<string>(txtpath.Split());
+                        words.Sort();
+                        BinarySearch.BinarySearchWord(words);
+                        break;
+                    default:
+                        Console.WriteLine("Enter the valid option!");
+                        break;
 
-                default:
-                    Console.WriteLine("Enter the valid option!");
-                    break;
+                }
+                Console.WriteLine("\nDo you want to continue");
+                flag = Console.ReadLine();
             }
-            Console.WriteLine("\nDo you want to continue");
-            Console.ReadLine();
         }
     }
 }
